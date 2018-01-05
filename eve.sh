717776df99c86bb134ce35b9ce3ed31f7ab75f62
Stoub <eve@no-cloud.fr>
2018-01-05 23:27:35 +0100

    fix version regexp

--- eve.sh
+++ eve.sh
@@ -4,12 +4,12 @@ VALUE=$(sh -c "${PLUGIN_SH_CMD}")
 
 cd eve
 
-grep -H -o -r "${PLUGIN_VARIABLE} [0-9a-fA-F]*$" * | while read LINE
+grep -H -o -r "${PLUGIN_VARIABLE} .*$" * | while read LINE
 do
 	FILE=`echo $LINE | cut -d":" -f1`
 	OLD=`echo $LINE | cut -d" " -f2`
 
 	echo "${FILE} : ${PLUGIN_VARIABLE} : ${OLD} -> ${VALUE}"
-	sed -i -e "s/${PLUGIN_VARIABLE} [0-9a-fA-F]*\$/${PLUGIN_VARIABLE} ${VALUE}/1" ${FILE}
+	sed -i -e "s/${PLUGIN_VARIABLE} .*\$/${PLUGIN_VARIABLE} ${VALUE}/1" ${FILE}
 
 done
